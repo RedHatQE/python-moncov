811871e3a286cb58b7b2fbc0bd268e0a70640711
dparalen <vetrisko@gmail.com>
2014-02-25 17:29:18 +0100

    adding some more sample src

--- misc/pushdown.py
+++ misc/pushdown.py
@@ -184,6 +184,11 @@ print "Hello, world!"
 def my_func(some, args):
     pass
 
+def parent_func(what, ever, args):
+    """some doc string"""
+    def baby_func():
+        pass
+
 class MyClass(object):
     class ANestedClass(object):
         def __init__(self):
@@ -197,7 +202,19 @@ class MyClass(object):
         print "Ahoj"
         print "Ahoj me"
 
+    @classmethod
+    def zoo_metdhod(cls):
+        def chicken_method():
+            def egg_method(*args, **kvs):
+                pass
+            egg_method(yolk='yellow')
+        chicken_method()
+
 print "Good Bye!"
+
+class Foo(object):
+    mcls = MyClass()
+
 print "Really, Good Bye!"
 
 '''
