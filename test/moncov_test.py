f348082999f78f7b1dedc61b12fa53a493b6e9b2
dparalen <vetrisko@gmail.com>
2014-04-10 19:05:25 +0200

    cleanup

--- test/moncov_test.py
+++ test/moncov_test.py
@@ -1,12 +1,8 @@
 import moncov
 import unittest
 import inspect
-import re
-import contextlib
 import logging
-import pprint
-from fractions import Fraction
-from tools import (traced, get_pyfilename_whitelist)
+from tools import get_pyfilename_whitelist, traced
 
 
 log = logging.getLogger(__name__)
@@ -18,7 +14,7 @@ class MoncovTest(unittest.TestCase):
     def setUpClass(cls):
         '''get reference to the db'''
         cls.db = moncov.conf.get_db(dbname="%s_db" % cls.__name__)
-        cls.whitelist = get_pyfilename_whitelist(__file__) # allow collecting this file only
+        cls.whitelist = get_pyfilename_whitelist(__file__)
         cls.blacklist = []
         pass
 
@@ -84,113 +80,3 @@ class MoncovTest(unittest.TestCase):
         assert response is not None
         self.assertEqual(response['value'], 1)
 
-    def test_04_no_data_to_measure_stats(self):
-        moncov.ctl.enable(db=self.db, whitelist=self.whitelist, blacklist=self.blacklist)
-        moncov.ctl.disable()
-        moncov.stats.update.update(self.db)
-        result = moncov.stats.simple.get_stats(db = self.db)
-        assert result == [], "nothing to measure"
-
-    def test_05__50percent_branch_rate(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            if value:
-                pass
-        tmp(True)
-        self.assertResultBranchRate(Fraction(1,2))
-
-
-    def test_06__100percent_branch_rate(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            if value:
-                pass
-        tmp(True); tmp(False)
-        self.assertResultBranchRate(Fraction(1,1))
-
-    def test_07_100percent_branch_rate_ifelse(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            if value:
-                pass
-            else:
-                None # FIXME pass in here breaks the test case
-        tmp(False); tmp(True);
-        self.assertResultBranchRate(Fraction(1,1))
-
-    def test_08_50percent_branch_rate_multiline_test(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            if False or \
-                    value:
-                pass
-        tmp(True)
-        self.assertResultBranchRate(Fraction(1,2))
-
-    def test_09_100percent_branch_rate_multiline_test(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            if False or \
-                    value:
-                pass
-            else:
-                None
-        tmp(True); tmp(False)
-        result = moncov.stats.simple.get_stats(db = self.db)
-        assert len(result) == 1, "just a single file measured"
-        status = result[0]
-        self.assertEqual(status.branch_rate, Fraction(2,2))
-
-    def test_10_50percent_branch_rate_For_multiline_empty(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(iterable):
-            for x in \
-                    iterable:
-                pass
-        tmp([])
-        self.assertResultBranchRate(Fraction(1,2))
-
-    def test_11_50percent_branch_rate_For_multiline_nonempty(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(iterable):
-            for x in \
-                    iterable:
-                pass
-        tmp([1,2])
-        self.assertResultBranchRate(Fraction(1,2))
-
-    def test_12_100percent_branch_rate_For_multiline(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(iterable):
-            for x in \
-                    iterable:
-                pass
-        tmp([]); tmp([1,2])
-        self.assertResultBranchRate(Fraction(2,2))
-
-    def test_13_50percent_branch_rate_While_false(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            i = 0
-            while i < value:
-                i += 1
-        tmp(0)
-        self.assertResultBranchRate(Fraction(1,2))
-
-    def test_14_50percent_branch_rate_While_true(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            i = 0
-            while i < value:
-                i += 1
-        tmp(3)
-        self.assertResultBranchRate(Fraction(1,2))
-
-    def test_15_100percent_branch_rate_While(self):
-        @traced(self.db, self.whitelist, self.blacklist)
-        def tmp(value):
-            i = 0
-            while i < value:
-                i += 1
-        tmp(0); tmp(3)
-        self.assertResultBranchRate(Fraction(2,2))
